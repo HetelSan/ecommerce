a7f33479a96f7479c5247fca82cde2274b63b8a7
hsantos <hetel.santos@gmail.com>
2018-08-29 12:49:28 -0300

    criamos pageadmin e tpl admin

--- index.php
+++ index.php
@@ -4,15 +4,26 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
+// Criando a rota(aquilo que é informado na URL do navegador)
 $app->get('/', function() {
 
 	// echo "OK";
 	$page = new Page();
+
+	$page->setTpl("index");
+
+});
+
+// Criando uma nova rota(aquilo que é informado na URL do navegador)
+$app->get('/Admin', function() {
+
+	$page = new PageAdmin();   // classe que irá produrar os templates corretos
 
 	$page->setTpl("index");
 
