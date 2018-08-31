7a61e60375ccdef4db3b0b5867330101c936d936
hsantos <hetel.santos@gmail.com>
2018-08-31 00:17:38 -0300

    recuperar senha

--- index.php
+++ index.php
@@ -13,8 +13,7 @@ $app = new Slim();
 
 $app->config('debug', true);
 
-// Criando a rota(aquilo que é informado na URL do navegador)
-$app->get('/', function() {
+$app->get('/', function () {
 
 	// echo "OK";
 	$page = new Page();
@@ -23,10 +22,9 @@ $app->get('/', function() {
 
 });
 
-// Criando uma nova rota(aquilo que é informado na URL do navegador)
-$app->get('/admin', function() {
+$app->get('/admin', function () {
 
-	User::verifyLogin();	
+	User::verifyLogin();
 
 	$page = new PageAdmin();   // classe que irá produrar os templates corretos
 
@@ -34,7 +32,7 @@ $app->get('/admin', function() {
 
 });
 
-$app->get('/admin/login', function() {
+$app->get('/admin/login', function () {
 
 	$page = new PageAdmin([
 		"header" => false,
@@ -42,11 +40,10 @@ $app->get('/admin/login', function() {
 	]);
 
 	$page->setTpl("login");
-	
+
 });
 
-// após informar o login, o método utilizado é post
-$app->post('/admin/login', function(){
+$app->post('/admin/login', function () {
 
 	User::login($_POST["login"], $_POST["password"]);
 
@@ -55,16 +52,174 @@ $app->post('/admin/login', function(){
 
 });
 
-// método para fazer o logout
-$app->get('/admin/logout', function(){
+$app->get('/admin/logout', function () {
 
 	User::logout();
 
 	header("Location: /admin/login");
 	exit;
-	
+
 });
 
+$app->get('/admin/users', function () {
+
+	User::verifyLogin();
+
+	$users = User::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users", array(
+		"users" => $users
+	));
+});
+
+$app->get('/admin/users/create', function () {
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-create");
+});
+
+$app->get('/admin/users/:iduser/delete', function ($iduser) {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("Location: /admin/users");
+	exit;
+
+});
+
+$app->get('/admin/users/:iduser', function ($iduser) {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-update", array(
+		"user" => $user->getValues()
+	));
+});
+
+$app->post('/admin/users/create', function () {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = isset($_POST["inadmin"]) ? 1 : 0;
+	$_POST['despassword'] = password_hash($_POST["despassword"], PASSWORD_DEFAULT, ["cost" => 12]);
+	$user->setData($_POST);
+
+	$user->save();
+
+	header("Location: /admin/users");
+	exit;
+
+});
+
+$app->post('/admin/users/:iduser', function ($iduser) {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = isset($_POST["inadmin"]) ? 1 : 0;
+
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");
+	exit;
+
+});
+
+$app->get('/admin/forgot', function () {
+
+	$page = new PageAdmin([
+		"header" => false,
+		"footer" => false
+	]);
+
+	$page->setTpl("forgot");
+
+});
+
+$app->post('/admin/forgot', function () {
+
+	$user = User::getForgot($_POST["e_mail"]);
+
+	header("Location: /admin/forgot/sent");
+	exit;
+
+});
+
+$app->get('/admin/forgot/sent', function () {
+
+	$page = new PageAdmin([
+		"header" => false,
+		"footer" => false
+	]);
+
+	$page->setTpl("forgot-sent");
+
+});
+
+$app->get('/admin/forgot/reset', function () {
+
+	$user = User::validForgotDecrypt($_GET["code"]);
+
+	$page = new PageAdmin([
+		"header" => false,
+		"footer" => false
+	]);
+
+	$page->setTpl("forgot-reset", array(
+		"name" => $user["desperson"],
+		"code" => $_GET["code"]
+	));
+
+});
+
+$app->post('/admin/forgot/reset', function () {
+
+	$forgot = User::validForgotDecrypt($_POST["code"]);   // recupera o código novamente para evitar hacker
+
+	User::setForgotUsed($forgot["idrecovery"]);
+
+	$user = new User();
+
+	$user->get((int)$forgot["iduser"]);
+
+	$password = password_hash($_POST["password"], PASSWORD_DEFAULT, [
+		"cost" => 12
+	]);
+
+	$user->setPassword($password);
+
+	$page = new PageAdmin([
+		"header" => false,
+		"footer" => false
+	]);
+
+	$page->setTpl("forgot-reset-success");
+
+});
 
 $app->run();
 
