a6f6e8fc3c8b07bed2fc2d3055aa3d72a206b0c5
hsantos <hetel.santos@gmail.com>
2018-09-01 16:36:52 -0300

    Categorias VS Produtos

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -59,7 +59,7 @@ class Category extends Model
         ));
 
         Category::updateFile();
-        
+
     }
 
     public static function updateFile()
@@ -70,10 +70,68 @@ class Category extends Model
         $html = [];
 
         foreach ($categories as $row) {
-            array_push($html, '<li><a href="/categories/'. $row['idcategory'] . '">' . $row['descategory'] . '</a></li>');
+            array_push($html, '<li><a href="/categories/' . $row['idcategory'] . '">' . $row['descategory'] . '</a></li>');
         }
 
         file_put_contents($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "categories-menu.html", implode('', $html));
+    }
+
+    public function getProducts($related = true)
+    {
+
+        $sql = new Sql();
+
+        if ($related === true) {
+
+            return $sql->select("
+                SELECT * FROM tb_products WHERE idproduct IN(
+                    SELECT a.idproduct
+                    from tb_products a
+                    inner join tb_productscategories b on a.idproduct = b.idproduct
+                    where b.idcategory = :idcategory
+                );
+            ", [
+                ':idcategory' => $this->getidcategory()
+            ]);
+        } else {
+
+            return $sql->select("
+                SELECT * FROM tb_products WHERE idproduct NOT IN(
+                    SELECT a.idproduct
+                    from tb_products a
+                    inner join tb_productscategories b on a.idproduct = b.idproduct
+                    where b.idcategory = :idcategory
+                );
+            ", [
+                ':idcategory' => $this->getidcategory()
+            ]);
+
+        }
+
+    }
+
+    public function addProduct(Product $product)
+    {
+
+        $sql = new Sql();
+
+        $sql->query("INSERT INTO tb_productscategories (idcategory, idproduct) VALUES (:idcategory, :idproduct)", [
+            ':idcategory' => $this->getidcategory(),
+            ':idproduct' => $product->getidproduct()
+        ]);
+
+    }
+
+    public function removeProduct(Product $product)
+    {
+
+        $sql = new Sql();
+
+        $sql->query("DELETE FROM tb_productscategories WHERE idcategory = :idcategory AND idproduct = :idproduct", [
+            ':idcategory' => $this->getidcategory(),
+            ':idproduct' => $product->getidproduct()
+        ]);
+
     }
 
 }
